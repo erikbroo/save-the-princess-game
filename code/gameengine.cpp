4641e9675ea91e2f23022f2fd19ca48fff50b604
elkhoury.anton <elkhoury.anton@gmail.com>
2012-04-06 09:38:24 +0000

    implemented loading of the level from *.spg file

--- code/gameengine.cpp
+++ code/gameengine.cpp
@@ -1,6 +1,92 @@
 #include "gameengine.h"
+#include <QFile>
 
 GameEngine::GameEngine(QObject *parent) :
     QObject(parent)
 {
+    initObjectsList();
+}
+
+bool GameEngine::loadLevel(QString levelFilename)
+{
+    QFile level(levelFilename);
+
+    if (!level.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        return false;
+    }
+
+    QByteArray buffer;
+    QVector<int> numberVector;
+
+    //read dimension
+    if(!level.atEnd())
+    {
+        buffer = level.readLine();
+        numberVector = extractNumber(buffer);
+        labirint.setDimension(numberVector.at(0),numberVector.at(1));
+
+    } else {
+        return false;
+    }
+
+    //read items in rows
+    for(int i = 0; i < labirint.getHeight(); i++)
+    {
+        numberVector.clear();
+        if (!level.atEnd())
+        {
+            buffer = level.readLine();
+            numberVector = extractNumber(buffer);
+
+            //add items to list
+            for(int j = 0; j < numberVector.size(); j++)
+            {
+                int id = numberVector.at(j);
+
+                labirint.setObject(i,j,id);
+            }
+
+        }
+    }
+
+    labirint.print();
+
+
+}
+
+void GameEngine::initObjectsList()
+{
+    int i = 0;
+
+    objectsList.insert(i++,"Grass");
+    objectsList.insert(i++,"Grass wall");
+    objectsList.insert(i++,"Water");
+    objectsList.insert(i++,"Stone wall");
+    objectsList.insert(i++,"Board");
+    objectsList.insert(i++,"Hero");
+    objectsList.insert(i++,"Princess");
+    objectsList.insert(i++,"Enemy1");
+}
+
+QVector<int> GameEngine::extractNumber(QByteArray line)
+{
+    QString number = "";
+    QVector<int> numbers;
+    int i = 0;
+    while (i < line.size()) {
+
+        if (QChar(line.at(i)).isDigit()) //find digit
+        {
+            while (i < line.size() && QChar(line.at(i)).isDigit()){
+                number.append(QChar(line.at(i)));
+                i++;
+            }
+            numbers.append(number.toInt());
+            number = "";
+        } else {
+           i++;
+        }
+    }
+    return numbers;
 }
