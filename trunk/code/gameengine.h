ed718510c3b586bcbeefd6fee8a4a2c24ff44e47
elkhoury.anton@gmail.com <elkhoury.anton@gmail.com@f0b6343e-7d61-a964-9723-7d461fbb88a7>
2012-04-06 09:38:24 +0000

    implemented loading of the level from *.spg file

--- trunk/code/gameengine.h
+++ trunk/code/gameengine.h
@@ -2,17 +2,28 @@
 #define GAMEENGINE_H
 
 #include <QObject>
+#include <QVector>
+#include "labirint.h"
 
 class GameEngine : public QObject
 {
     Q_OBJECT
 public:
     explicit GameEngine(QObject *parent = 0);
+    bool loadLevel(QString levelFilename);
     
 signals:
     
 public slots:
-    
+
+private:
+    Labirint labirint;
+
+    //list to identify objects using their id's
+    QMap<int,QString> objectsList;
+
+    void initObjectsList();
+    QVector<int> extractNumber(QByteArray line);
 };
 
 #endif // GAMEENGINE_H
